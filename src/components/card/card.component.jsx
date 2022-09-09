4b99f556fa8886c46b57a40754ad6f2dc0484952
Md Rabi Ul Uslam <58680120+rabiulislam007@users.noreply.github.com>
2022-09-10 01:37:25 +0600

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,7 +6,7 @@ const Card = ({ monster }) => {
     <div className='card-container' key={id}>
       <img
         alt={`monster ${name}`}
-        src={`https://robohash.org/${id}?set=set2`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
       />
       <h2>{name}</h2>
       <p>{email}</p>
